6a460402e82f497868127d61a2fbb2a3697fe941
NatuKumo <yakikazu6@gmail.com>
2019-03-22 10:26:38 +0900

    SendGrid Setting

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,7 +18,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save     #ここでmodelでactiveトークンとダイジェストが決められる。
-      @user.send_activation_email
+      @user.send_activation_email  #modelに書いてある。
       flash[:info] = "Please check your email to activate your account."
       redirect_to root_url     
     else
